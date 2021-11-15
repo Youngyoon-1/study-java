7ab086a4e72cd8367811b4716dc2e42611fac8a6
yoon <duddbs774@gmail.com>
2021-11-15 18:01:14 +0900

    test: StringCalc 클래스 싱글톤패턴테스트케이스 추가

--- stringCalc/src/main/java/StringCalc.java
+++ stringCalc/src/main/java/StringCalc.java
@@ -1,11 +1,12 @@
 public class StringCalc {
-    static private final StringCalc stringCalc = new StringCalc();
+    private static final StringCalc stringCalc = new StringCalc();
 
     private StringCalc(){}
 
-    static StringCalc getInstance(){
+    public static StringCalc getInstance(){
         return stringCalc;
     }
+
     public int calc(String str){
         str = str.replaceAll(" ","");   //공백제거
         return 0;
--- stringCalc/src/test/java/StringCalcTest.java
+++ stringCalc/src/test/java/StringCalcTest.java
@@ -3,13 +3,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class StringCalcTest {
 
+    StringCalc calc;
+
     @BeforeEach
-    void setUp(){
-        StringCalc calc = StringCalc.getInstance();
-    }
+    void setUp(){ calc = StringCalc.getInstance(); }
 
     @DisplayName("replacAll로 공백제거")
     @Test
@@ -17,5 +18,10 @@ public class StringCalcTest {
         assertThat(" + 2  2  2+  23".replaceAll(" ","")).isEqualTo("+222+23");
     }
 
+    @DisplayName("싱글톤패턴 테스트")
+    @Test
+    void isSingleton(){
+        assertThat(calc).isEqualTo(StringCalc.getInstance());
+    }
 
 }
