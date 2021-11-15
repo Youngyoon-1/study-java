313c2e284a21270dd2b7205ea344786b910fec7c
yoon <duddbs774@gmail.com>
2021-11-15 16:47:58 +0900

    refactor: ValidationUtil 메소드 static으로 변경 및 생성자 private으로 변경

--- stringCalc/src/main/java/ValidationUtil.java
+++ stringCalc/src/main/java/ValidationUtil.java
@@ -2,18 +2,20 @@ import java.util.List;
 
 public class ValidationUtil {
 
-    public void checkFirstIdx(String s) {
+    private ValidationUtil() throws InstantiationException { throw new InstantiationException("ValidationUtil");}
+
+    public static void checkFirstIdx(String s) {
         if(!(s.charAt(0) >= '0' && s.charAt(0) <='9'))
             throw new IllegalArgumentException("첫입력자는 숫자여야합니다.");
     }
 
-    public void checkLastIdx(String s) {
+    public static void checkLastIdx(String s) {
         int lastIdx = s.length() - 1;
         if(!(s.charAt(lastIdx) >= '0' && s.charAt(lastIdx) <= '9'))
             throw new IllegalArgumentException("마지막 입력자는 숫자여야합니다.");
     }
 
-    public void checkOp(String regex, String s) {
+    public static void checkOp(String regex, String s) {
         s = s.replaceAll("[0-9]","");
         s = s.replaceAll(regex,"");
         if(!s.equals(""))
--- stringCalc/src/test/java/ValidationUtilTest.java
+++ stringCalc/src/test/java/ValidationUtilTest.java
@@ -10,17 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class ValidationUtilTest {
-    ValidationUtil validationUtil;
-
-    @BeforeEach
-    void setUp(){
-        validationUtil = new ValidationUtil();
-    }
 
     @DisplayName("입력값 첫번째가 숫자일 경우")
     @Test
     void checkFirstIdx(){
-        validationUtil.checkFirstIdx("1+1");
+        ValidationUtil.checkFirstIdx("1+1");
     }
 
     @DisplayName("입력값 첫번째가 숫자가 아닐 경우")
@@ -28,14 +22,14 @@ public class ValidationUtilTest {
     @ValueSource(strings = {"+11","-1","-","*9"})
     void checkFirstIdx(String s){
         assertThatIllegalArgumentException().isThrownBy(()->{
-            validationUtil.checkFirstIdx(s);
+            ValidationUtil.checkFirstIdx(s);
         }).withMessageMatching("첫입력자는 숫자여야합니다.");
     }
 
     @DisplayName("입력값 마지막이 숫자일 경우")
     @Test
     void checkLastIdx(){
-        validationUtil.checkLastIdx("1+1");
+        ValidationUtil.checkLastIdx("1+1");
     }
 
     @DisplayName("입력값 마지막이 숫자가 아닐 경우")
@@ -43,14 +37,14 @@ public class ValidationUtilTest {
     @ValueSource(strings={"11+","1-","1-1-"})
     void checkLastIdx(String s){
         assertThatIllegalArgumentException().isThrownBy(()->{
-            validationUtil.checkLastIdx(s);
+            ValidationUtil.checkLastIdx(s);
         }).withMessageMatching("마지막 입력자는 숫자여야합니다.");
     }
 
     @DisplayName("입력값중 사칙연산자이외의 연산자가 없는 경우")
     @Test
     void checkOp(){
-        validationUtil.checkOp("[\\+|\\*|/|-]","1+1");
+        ValidationUtil.checkOp("[\\+|\\*|/|-]","1+1");
     }
 
     @DisplayName("사칙연산자이외의 연산자가 있을 경우 테스트")
@@ -58,7 +52,7 @@ public class ValidationUtilTest {
     @ValueSource(strings = {"1%1","2!2","2+3=5"})
     void checkOp(String s){
         assertThatIllegalArgumentException().isThrownBy(()->{
-            validationUtil.checkOp("[\\+|\\*|/|-]",s);
+            ValidationUtil.checkOp("[\\+|\\*|/|-]",s);
         }).withMessageMatching("연산자가 올바르지 않습니다.");
     }
 }
