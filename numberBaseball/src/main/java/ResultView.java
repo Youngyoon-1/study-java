18ff8a2c780e979317204004840ebac87e58c4e9
yoon <duddbs774@gmail.com>
2021-11-19 02:04:44 +0900

    feat: ResultView 클래스안에 print 메서드 (게임종료 및 재시작 선택 멘트) 오버로딩으로 구현

--- numberBaseball/src/main/java/ResultView.java
+++ numberBaseball/src/main/java/ResultView.java
@@ -4,4 +4,10 @@ public class ResultView {
     static void print(List<String> result){
         result.stream().forEach(score -> System.out.printf("%s ",score));
     }
+
+    //오버로딩
+    static void print(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
