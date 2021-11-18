b130f6c30406383b82d2116432378ee48dcbc1ec
yoon <duddbs774@gmail.com>
2021-11-19 02:41:53 +0900

    feat: GameManager 클래스 안에 play 메서드(게임진행기능)구현

--- numberBaseball/src/main/java/GameManager.java
+++ numberBaseball/src/main/java/GameManager.java
@@ -1,4 +1,8 @@
+import util.FormatUtil;
+import util.NumberUtil;
+
 import java.util.List;
+import java.util.Scanner;
 
 public class GameManager {
 
@@ -20,4 +24,22 @@ public class GameManager {
         }
         return false;
     }
+
+    public void play(Scanner sc, List<Integer> actual){
+        boolean is3Strike = false;
+        while(!is3Strike){
+            List<String> result = Score.of(actual,FormatUtil.convertStr(InputView.receiveNos(sc)));
+            ResultView.print(result);
+            is3Strike = check3Strike(result);
+        }
+    }
+
+//    public void init(Scanner sc){
+//        boolean gameToken = true;
+//        while(gameToken){
+//            play(sc, NumberUtil.makeNos());
+//            ResultView.print();
+//            gameToken = checkNo(InputView.receiveNo(sc));
+//        }
+//    }
 }
