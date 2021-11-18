0d42b2b0c806643562f9c2550ef9190afdf43a5e
yoon <duddbs774@gmail.com>
2021-11-19 02:08:27 +0900

    feat: InputView 클래스 안에 receiveNo 메서드(재시작 입력값 받는 기능)구현

--- numberBaseball/src/main/java/InputView.java
+++ numberBaseball/src/main/java/InputView.java
@@ -5,4 +5,9 @@ public class InputView {
         System.out.print("숫자를 입력해 주세요 : ");
         return sc.nextLine();
     }
+
+    static String receiveNo(Scanner sc){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return sc.nextLine();
+    }
 }
