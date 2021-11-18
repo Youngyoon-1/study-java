9ab4f2a29105c24622609060ceffc8e07bfe2033
yoon <duddbs774@gmail.com>
2021-11-18 22:01:40 +0900

    feat: enum Score 클래스에 NOTHING 구현완료

--- numberBaseball/src/main/java/Score.java
+++ numberBaseball/src/main/java/Score.java
@@ -3,10 +3,20 @@ import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public enum Score {
-    BALL((a,b) -> ((int) a.stream()
-                .filter(no -> b.stream().anyMatch(Predicate.isEqual(no)))
-                .count() - (int)IntStream.range(0, a.size()).filter(i -> a.get(i)==b.get(i)).count())),
-    STRIKE((a,b) -> (int)IntStream.range(0,a.size()).filter(i -> a.get(i)==b.get(i)).count());
+    BALL((a,b) -> {
+        return (int) a.stream()
+                    .filter(no -> b.stream().anyMatch(Predicate.isEqual(no)))
+                    .count() - (int)IntStream.range(0, a.size()).filter(i -> a.get(i)==b.get(i)).count();
+    }),
+
+    STRIKE((a,b) -> (int)IntStream.range(0,a.size()).filter(i -> a.get(i)==b.get(i)).count()),
+
+    NOTHING((a,b) -> {
+        if((int)a.stream().filter(no -> b.stream().anyMatch(Predicate.isEqual(no))).count() == 0){
+            return 1;
+        }
+        return 0;
+    });
 
     Formula formula;
 
--- numberBaseball/src/test/java/ScoreTest.java
+++ numberBaseball/src/test/java/ScoreTest.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoreTest {
+
     @DisplayName("BALL 계산 테스트 ")
     @ParameterizedTest
     @CsvSource(value={"123:123:0","123:316:2","456:765:2","456:789:0","123:345:1"},delimiter=':')
@@ -26,5 +27,15 @@ public class ScoreTest {
         List<Integer> actual = Arrays.stream(s1.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
         List<Integer> expected = Arrays.stream(s2.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
         assertThat(Score.STRIKE.formula.calc(actual,expected)).isEqualTo(result);
+    }
+
+    @DisplayName("Nothing 계산 테스트")
+    @ParameterizedTest
+    @CsvSource(value={"123:456:1","123:123:0","123:345:0","567:765:0","456:789:1"},delimiter = ':')
+    void nothing(String s1, String s2, int result){
+        List<Integer> actual = Arrays.stream(s1.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        List<Integer> expected = Arrays.stream(s2.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+
+        assertThat(Score.NOTHING.formula.calc(actual, expected)).isEqualTo(result);
     }
 }
