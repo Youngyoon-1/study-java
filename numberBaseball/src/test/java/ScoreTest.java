663eefaf102ff322f04962910d60bf0e90e5b020
yoon <duddbs774@gmail.com>
2021-11-18 23:53:36 +0900

    test: ScoreTest 테스트코드 리팩토링

--- numberBaseball/src/test/java/ScoreTest.java
+++ numberBaseball/src/test/java/ScoreTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,12 +10,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoreTest {
 
+    private List<Integer> converter(String s){
+        return Arrays.stream(s.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+    }
+
     @DisplayName("BALL 계산 테스트 ")
     @ParameterizedTest
     @CsvSource(value={"123:123:0","123:316:2","456:765:2","456:789:0","123:345:1"},delimiter=':')
     void ball(String s1, String s2, int result){
-        List<Integer> actual = Arrays.stream(s1.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-        List<Integer> expected = Arrays.stream(s2.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        List<Integer> actual = converter(s1);
+        List<Integer> expected = converter(s2);
 
         assertThat(Score.BALL.formula.calc(actual, expected)).isEqualTo(result);
     }
@@ -25,8 +28,8 @@ public class ScoreTest {
     @ParameterizedTest
     @CsvSource(value={"123:123:3","123:923:2","123:145:1","123:456:0"},delimiter = ':')
     void strike(String s1, String s2, int result){
-        List<Integer> actual = Arrays.stream(s1.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-        List<Integer> expected = Arrays.stream(s2.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        List<Integer> actual = converter(s1);
+        List<Integer> expected = converter(s2);
 
         assertThat(Score.STRIKE.formula.calc(actual,expected)).isEqualTo(result);
     }
@@ -35,8 +38,8 @@ public class ScoreTest {
     @ParameterizedTest
     @CsvSource(value={"123:456:1","123:123:0","123:345:0","567:765:0","456:789:1"},delimiter = ':')
     void nothing(String s1, String s2, int result){
-        List<Integer> actual = Arrays.stream(s1.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-        List<Integer> expected = Arrays.stream(s2.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        List<Integer> actual = converter(s1);
+        List<Integer> expected = converter(s2);
 
         assertThat(Score.NOTHING.formula.calc(actual, expected)).isEqualTo(result);
     }
@@ -45,11 +48,10 @@ public class ScoreTest {
     @ParameterizedTest
     @CsvSource(value={"123:123:3스트라이크","123:132:2볼,1스트라이크","123:167:1스트라이크","123:256:1볼","123:456:낫싱","123:789:낫싱"},delimiter=':')
     void calcScore(String s1, String s2, String s3){
-        List<Integer> actual = Arrays.stream(s1.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-        List<Integer> expected = Arrays.stream(s2.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        List<Integer> actual = converter(s1);
+        List<Integer> expected = converter(s2);
         List<String> result = Arrays.stream(s3.split(",")).collect(Collectors.toList());
 
         assertThat(Score.of(expected,actual)).isEqualTo(result);
-
     }
 }
