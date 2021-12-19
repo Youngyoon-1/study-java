32be40486479b3ab3ec02bcfafcfdaf03c04c5a2
yoon <duddbs774@gmail.com>
2021-12-19 18:53:31 +0900

    feat: 입력값의 좌표가 중복일 경우 예외 발생 기능 구현

--- tdd/coordinateCalculator/src/main/java/coordinateCalculator/utils/ValidationUtil.java
+++ tdd/coordinateCalculator/src/main/java/coordinateCalculator/utils/ValidationUtil.java
@@ -4,15 +4,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidationUtil {
+    static final String ERROR_PREFIX = "[ERROR] ";
+    static final String ERROR_INVALID_FORM = "형식에 맞도록 입력해주세요.";
+    static final String ERROR_DUPLICATION = "서로 다른 좌표값을 입력해주세요.";
+    public static final String COORDINATE_DELIMITER = "-";
+    public static final int FIRST_INPUT_INDEX = 0;
+    public static final int SECOND_INPUT_INDEX = 1;
+
     public static void checkForm(String input) {
         String regex = "\\([1-9]*[0-9],[1-9]*[0-9]\\)-\\([1-9]*[0-9],[1-9]*[0-9]\\)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException("[ERROR] 형식에 맞도록 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_PREFIX + ERROR_INVALID_FORM);
         }
-        // if (Pattern.matches(regex, input)) {
-        //     throw new IllegalArgumentException("[ERROR] 형식에 맞도록 입력해주세요.");
-        // }
+    }
+
+    public static void checkDuplication(String input) {
+        String[] values = input.split(COORDINATE_DELIMITER);
+        if (values[FIRST_INPUT_INDEX].equals(values[SECOND_INPUT_INDEX])) {
+            throw new IllegalArgumentException(ERROR_PREFIX + ERROR_DUPLICATION);
+        }
     }
 }
--- tdd/coordinateCalculator/src/test/java/coordinateCalculator/utils/ValidationUtilTest.java
+++ tdd/coordinateCalculator/src/test/java/coordinateCalculator/utils/ValidationUtilTest.java
@@ -1,5 +1,6 @@
 package coordinateCalculator.utils;
 
+import static coordinateCalculator.utils.ValidationUtil.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,13 +9,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidationUtilTest {
-    private static final String ERROR_PREFIX = "[ERROR]";
 
     @DisplayName("입력값이 형식에 맞지않을 경우 예외발생")
     @ParameterizedTest
     @ValueSource(strings = {"(1,2)+(1,2)", "(1,2 - 1,3)", "1 - 2", "(1,3),(1,5)"})
     void checkInvalidForm(String invalidInput) {
-        assertThatThrownBy(() -> ValidationUtil.checkForm(invalidInput)).hasMessageContaining(ERROR_PREFIX);
+        assertThatThrownBy(() -> ValidationUtil.checkForm(invalidInput)).hasMessage(ERROR_PREFIX + ERROR_INVALID_FORM);
     }
 
     @DisplayName("입력값이 형식에 맞을 경우")
@@ -24,11 +24,10 @@ public class ValidationUtilTest {
         assertDoesNotThrow(() -> ValidationUtil.checkForm(validInput));
     }
 
-    // @DisplayName("입력값이 형식에 맞지않을 경우 예외발생")
-    // @Test
-    // void checkDuplication() {
-    //     assertThatThrownBy(() -> {
-    //         ValidationUtil.checkDuplication((1, 2)-(1, 2));
-    //     })
-    // }
+    @DisplayName("입력값 중 두 좌표가 중복일 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"(0,9)-(0,9)", "(4,5)-(4,5)"})
+    void checkDuplication(String input) {
+        assertThatThrownBy(() -> ValidationUtil.checkDuplication(input)).hasMessage(ERROR_PREFIX + ERROR_DUPLICATION);
+    }
 }
