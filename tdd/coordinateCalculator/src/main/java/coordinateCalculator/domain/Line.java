cd9e92f34e0a8b4861497fcb37f02508689f08fa
yoon <duddbs774@gmail.com>
2021-12-20 21:57:40 +0900

    feat: 두개의 좌표 사이 거리 계산 기능 구현

---
+++ tdd/coordinateCalculator/src/main/java/coordinateCalculator/domain/Line.java
@@ -0,0 +1,22 @@
+package coordinateCalculator.domain;
+
+public class Line {
+    public static final String COORDINATE_DELIMITER = "-";
+    public static final int FIRST_COORDINATE_INDEX = 0;
+    public static final int SECOND_COORDINATE_INDEX = 1;
+
+    private final Coordinate firstCoordinate;
+    private final Coordinate secondCoordinate;
+
+    public Line(String input) {
+        String[] coordinates = input.split(COORDINATE_DELIMITER);
+        firstCoordinate = new Coordinate(coordinates[FIRST_COORDINATE_INDEX]);
+        secondCoordinate = new Coordinate(coordinates[SECOND_COORDINATE_INDEX]);
+    }
+
+    public double length() {
+        int a = firstCoordinate.getX() - secondCoordinate.getX();
+        int b = firstCoordinate.getY() - secondCoordinate.getY();
+        return Math.sqrt(a * a + b * b);
+    }
+}
