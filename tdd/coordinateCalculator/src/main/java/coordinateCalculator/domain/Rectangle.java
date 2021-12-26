1dbe6ebe058fef4adc9cddad8ae99bea3410261c
yoon <duddbs774@gmail.com>
2021-12-26 21:00:23 +0900

    feat: 사각형 생성시 점 갯수가 틀릴 경우 예외 발생 기능 구현

--- tdd/coordinateCalculator/src/main/java/coordinateCalculator/domain/Rectangle.java
+++ tdd/coordinateCalculator/src/main/java/coordinateCalculator/domain/Rectangle.java
@@ -1,8 +1,12 @@
 package coordinateCalculator.domain;
 
 public class Rectangle extends AbstractFigure {
+    public static final int POINT_COUNT = 4;
+    public static final String ERROR_INVALID_POINT_COUNT = "[ERROR] 사각형은 " + POINT_COUNT + "개의 점으로 이뤄져 있어야 합니다.";
+
     public Rectangle(Points points) {
         super(points);
+        checkValidPoints(points);
     }
 
     @Override
@@ -12,5 +16,8 @@ public class Rectangle extends AbstractFigure {
 
     @Override
     public void checkValidPoints(Points points) {
+        if (points().isInvalidPoints(POINT_COUNT)) {
+            throw new IllegalArgumentException(ERROR_INVALID_POINT_COUNT);
+        }
     }
 }
--- tdd/coordinateCalculator/src/test/java/coordinateCalculator/domain/RectangleTest.java
+++ tdd/coordinateCalculator/src/test/java/coordinateCalculator/domain/RectangleTest.java
@@ -1,5 +1,6 @@
 package coordinateCalculator.domain;
 
+import static coordinateCalculator.domain.Rectangle.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
@@ -14,5 +15,26 @@ public class RectangleTest {
         Points points
             = new Points(Arrays.asList(Point.of(10, 10), Point.of(22, 10), Point.of(22, 18), Point.of(10, 18)));
         assertThat(new Rectangle(points).area()).isEqualTo(96);
+    }
+
+    @DisplayName("점이 세 개일 경우 예외 발생")
+    @Test
+    void threePoints() {
+        Points points
+            = new Points(Arrays.asList(Point.of(10, 10), Point.of(22, 10), Point.of(22, 18)));
+        assertThatThrownBy(() -> new Rectangle(points)).hasMessage(ERROR_INVALID_POINT_COUNT);
+    }
+
+    @DisplayName("점이 다섯 개일 경우 예외 발생")
+    @Test
+    void fivePoints() {
+        Points points = new Points(Arrays.asList(
+            Point.of(10, 10),
+            Point.of(22, 10),
+            Point.of(22, 18),
+            Point.of(10, 18),
+            Point.of(20, 20)
+        ));
+        assertThatThrownBy(() -> new Rectangle(points)).hasMessage(ERROR_INVALID_POINT_COUNT);
     }
 }
