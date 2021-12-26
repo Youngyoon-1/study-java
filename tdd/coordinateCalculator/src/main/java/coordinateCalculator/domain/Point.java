758270bd6e7c0f58962080158a1d9b00fb10e6fb
yoon <duddbs774@gmail.com>
2021-12-26 16:37:08 +0900

    feat: 두 점 사이 길이 구하기 기능 구현

--- tdd/coordinateCalculator/src/main/java/coordinateCalculator/domain/Point.java
+++ tdd/coordinateCalculator/src/main/java/coordinateCalculator/domain/Point.java
@@ -5,18 +5,17 @@ public class Point {
     public static final int COORDINATE_MAXIMUM_NUMBER = 24;
     public static final String ERROR_INVALID_COORDINATE
         = "[ERROR] " + COORDINATE_MINIMUM_NUMBER + "부터 " + COORDINATE_MAXIMUM_NUMBER + "까지의 자연수만 허용됩니다.";
-
     private final int x;
     private final int y;
 
     public Point(int xCoordinate, int yCoordinate) {
+        checkValidCoordinate(xCoordinate);
+        checkValidCoordinate(yCoordinate);
         this.x = xCoordinate;
         this.y = yCoordinate;
     }
 
     public static Point of(int xCoordinate, int yCoordinate) {
-        checkValidCoordinate(xCoordinate);
-        checkValidCoordinate(yCoordinate);
         return new Point(xCoordinate, yCoordinate);
     }
 
@@ -24,5 +23,20 @@ public class Point {
         if (coordinate < COORDINATE_MINIMUM_NUMBER || coordinate > COORDINATE_MAXIMUM_NUMBER) {
             throw new IllegalArgumentException(ERROR_INVALID_COORDINATE);
         }
+    }
+
+    public double calculateDistance(Point other) {
+        int xDifference = getXDifference(other);
+        int yDifference = getYDifference(other);
+
+        return Math.sqrt(xDifference * xDifference + yDifference * yDifference);
+    }
+
+    private int getYDifference(Point other) {
+        return y - other.y;
+    }
+
+    private int getXDifference(Point other) {
+        return x - other.x;
     }
 }
--- tdd/coordinateCalculator/src/test/java/coordinateCalculator/domain/PointTest.java
+++ tdd/coordinateCalculator/src/test/java/coordinateCalculator/domain/PointTest.java
@@ -2,8 +2,10 @@ package coordinateCalculator.domain;
 
 import static coordinateCalculator.domain.Point.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,4 +16,17 @@ public class PointTest {
     void invalidPoint(int xCoordinate, int yCoordinate) {
         assertThatThrownBy(() -> Point.of(xCoordinate, yCoordinate)).hasMessage(ERROR_INVALID_COORDINATE);
     }
+
+    @DisplayName("점 생성시 지정 값 범위를 벗어나지 않는 경우(1 ~ 24)")
+    @Test
+    void validPoint() {
+        assertDoesNotThrow(() -> Point.of(1, 24));
+    }
+
+    @DisplayName("두 점 사이 거리 구하기")
+    @Test
+    void calculateDistance() {
+        assertThat(Point.of(10, 10).calculateDistance(Point.of(14, 15))).isEqualTo(6.403124, offset(0.000001d));
+    }
+
 }
