9107fc0dfc868925c6d8e2eedd1be45592ca70b2
yoon <duddbs774@gmail.com>
2022-01-03 21:21:51 +0900

    feat: 이름 입력값이 양식에 맞지 않을 경우 예외 발생 기능 구현

--- tdd/blackJack/src/main/java/blackJack/veiw/InputView.java
+++ tdd/blackJack/src/main/java/blackJack/veiw/InputView.java
@@ -5,20 +5,34 @@ import blackJack.domain.Player;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
     public static final String ERROR_DUPLICATE = "[ERROR] 중복된 이름은 사용 할 수 없습니다.";
+    public static final String ERROR_FORMAT = "[ERROR] 양식에 맞춰서 입력해주세요.";
+    public static final String INPUT_DELIMITER = ",";
 
     public static List<Player> createPlayer(String s) {
-        return Arrays.stream(s.split(",")).map(Player::new).collect(Collectors.toList());
+        return Arrays.stream(s.split(INPUT_DELIMITER)).map(Player::new).collect(Collectors.toList());
     }
 
     public static void checkDuplication(String s) {
-        String[] names = s.split(",");
+        String[] names = s.split(INPUT_DELIMITER);
 
         if (new HashSet<>(Arrays.asList(names)).size() != names.length) {
             throw new IllegalArgumentException(ERROR_DUPLICATE);
+        }
+    }
+
+    public static void checkFormat(String s) {
+        String regex = "[a-zA-Z0-9]+(,[a-zA-Z0-9]+)+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ERROR_FORMAT);
         }
     }
 }
--- tdd/blackJack/src/test/java/blackJack/veiw/InputViewTest.java
+++ tdd/blackJack/src/test/java/blackJack/veiw/InputViewTest.java
@@ -3,11 +3,14 @@ package blackJack.veiw;
 import blackJack.domain.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static blackJack.veiw.InputView.ERROR_DUPLICATE;
+import static blackJack.veiw.InputView.ERROR_FORMAT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -23,8 +26,21 @@ public class InputViewTest {
 
     @DisplayName("이름 중복 체크")
     @Test
-    void hasDuplication() {
+    void checkDuplication() {
         assertThatThrownBy(() -> InputView.checkDuplication("jadal,jadal")).hasMessage(ERROR_DUPLICATE);
         assertDoesNotThrow(() -> InputView.checkDuplication("jadal,jayho"));
+    }
+
+    @DisplayName("유효하지 않은 양식")
+    @ParameterizedTest
+    @ValueSource(strings = {"jadal", "jadal:jayho", "jadal.jayho", "jadal;jayho"})
+    void invalidFormat(String s) {
+        assertThatThrownBy(() -> InputView.checkFormat(s)).hasMessage(ERROR_FORMAT);
+    }
+
+    @DisplayName("유효한 양식")
+    @Test
+    void validFormat() {
+        assertDoesNotThrow(() -> InputView.checkFormat("jadal,jayho"));
     }
 }
