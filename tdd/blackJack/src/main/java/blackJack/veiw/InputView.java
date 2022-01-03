8deb30cbe6d2a3e7545d0fdbe361118b0c92168e
yoon <duddbs774@gmail.com>
2022-01-03 19:59:43 +0900

    feat: 중복된 이름 입력시 예외 발생 기능 구현

--- tdd/blackJack/src/main/java/blackJack/veiw/InputView.java
+++ tdd/blackJack/src/main/java/blackJack/veiw/InputView.java
@@ -3,12 +3,22 @@ package blackJack.veiw;
 import blackJack.domain.Player;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
+    public static final String ERROR_DUPLICATE = "[ERROR] 중복된 이름은 사용 할 수 없습니다.";
 
     public static List<Player> createPlayer(String s) {
         return Arrays.stream(s.split(",")).map(Player::new).collect(Collectors.toList());
+    }
+
+    public static void checkDuplication(String s) {
+        String[] names = s.split(",");
+
+        if (new HashSet<>(Arrays.asList(names)).size() != names.length) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE);
+        }
     }
 }
--- tdd/blackJack/src/test/java/blackJack/veiw/InputViewTest.java
+++ tdd/blackJack/src/test/java/blackJack/veiw/InputViewTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static blackJack.veiw.InputView.ERROR_DUPLICATE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class InputViewTest {
     @DisplayName("입력값으로 플레이어 생성")
@@ -16,5 +19,12 @@ public class InputViewTest {
         List<Player> actual = InputView.createPlayer("jadal,jayho");
         List<Player> expected = Arrays.asList(new Player("jadal"), new Player("jayho"));
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("이름 중복 체크")
+    @Test
+    void hasDuplication() {
+        assertThatThrownBy(() -> InputView.checkDuplication("jadal,jadal")).hasMessage(ERROR_DUPLICATE);
+        assertDoesNotThrow(() -> InputView.checkDuplication("jadal,jayho"));
     }
 }
