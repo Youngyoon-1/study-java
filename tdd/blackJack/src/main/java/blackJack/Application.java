9fc64211d3b3e4848e0762ff375f19029c50e533
yoon <duddbs774@gmail.com>
2022-01-03 18:53:43 +0900

    피드백 반영해서 처음부터 다시 구현

--- tdd/blackJack/src/main/java/blackJack/Application.java
+++
@@ -1,9 +0,0 @@
-package blackJack;
-
-import blackJack.controller.BlackJackController;
-
-public class Application {
-    public static void main(String[] args) {
-        new BlackJackController().run();
-    }
-}
