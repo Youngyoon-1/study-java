0585ee0d32f553280beb486b0eebfa822f1f7c90
yoon <duddbs774@gmail.com>
2022-01-04 04:10:12 +0900

    feat: 플레이어 이름과 배팅금액 입력 받기 구현

---
+++ tdd/blackJack/src/main/java/blackJack/Application.java
@@ -0,0 +1,9 @@
+package blackJack;
+
+import blackJack.veiw.InputView;
+
+public class Application {
+    public static void main(String[] args) {
+        InputView.initPlayer();
+    }
+}
--- tdd/blackJack/src/main/java/blackJack/veiw/InputView.java
+++ tdd/blackJack/src/main/java/blackJack/veiw/InputView.java
@@ -1,11 +1,13 @@
 package blackJack.veiw;
 
+import blackJack.domain.BettingAmount;
 import blackJack.domain.Player;
 import blackJack.domain.PlayerName;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -15,20 +17,57 @@ public class InputView {
     public static final String ERROR_FORMAT = "[ERROR] 양식에 맞춰서 입력해주세요.";
     public static final String INPUT_DELIMITER = ",";
     public static final String ERROR_INVALID_AMOUNT = "[ERROR] 숫자를 입력해주세요.";
+    public static final String SPACE = " ";
+    public static final String EMPTY = "";
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void initPlayer() {
+        inputBettingAmount(createPlayer());
+    }
+
+    private static void inputBettingAmount(List<Player> players) {
+        players.forEach(InputView::setBettingAmount);
+    }
+
+    private static void setBettingAmount(Player player) {
+        System.out.println(player + "의 배팅 금액은?");
+        String input = readLine();
+        try {
+            checkAmount(input);
+            player.setBettingAmount(new BettingAmount(Integer.parseInt(input)));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setBettingAmount(player);
+        }
+    }
+
+    private static List<Player> createPlayer() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        String input = readLine();
+        try {
+            checkValidate(input);
+            return createPlayer(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return createPlayer();
+        }
+    }
+
+    private static void checkValidate(String input) {
+        checkFormat(input);
+        checkDuplication(input);
+    }
+
+    private static String readLine() {
+        return scanner.nextLine().replace(SPACE, EMPTY);
+    }
 
     public static List<Player> createPlayer(String s) {
         return Arrays.stream(s.split(INPUT_DELIMITER))
                 .map(PlayerName::new)
                 .map(Player::new)
                 .collect(Collectors.toList());
-    }
-
-    public static void checkDuplication(String s) {
-        String[] names = s.split(INPUT_DELIMITER);
-
-        if (new HashSet<>(Arrays.asList(names)).size() != names.length) {
-            throw new IllegalArgumentException(ERROR_DUPLICATE);
-        }
     }
 
     public static void checkFormat(String s) {
@@ -38,6 +77,14 @@ public class InputView {
 
         if (!matcher.matches()) {
             throw new IllegalArgumentException(ERROR_FORMAT);
+        }
+    }
+
+    public static void checkDuplication(String s) {
+        String[] names = s.split(INPUT_DELIMITER);
+
+        if (new HashSet<>(Arrays.asList(names)).size() != names.length) {
+            throw new IllegalArgumentException(ERROR_DUPLICATE);
         }
     }
 
